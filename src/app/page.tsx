7fcc16acec30621420534aec6965534774f56e30
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-03-12 12:18:14 +0530

    Clerk SignUp setup is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
-import { Button }  from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
-     <h1>Home Page</h1>
-     <Button>Sign in</Button>
-   
-    </>
-   
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   );
 }
